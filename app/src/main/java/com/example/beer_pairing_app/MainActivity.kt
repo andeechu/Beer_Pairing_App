9ad2520e9b50eede8033ef4e7fa015f6d05f1753
andeechu <andychuabsurd@gmail.com>
2019-12-08 22:37:02 -0800

    added navigation controller and added main menu fragment

--- app/src/main/java/com/example/beer_pairing_app/MainActivity.kt
+++ app/src/main/java/com/example/beer_pairing_app/MainActivity.kt
@@ -2,11 +2,24 @@ package com.example.beer_pairing_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.example.beer_pairing_app.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        return navController.navigateUp()
     }
 }
