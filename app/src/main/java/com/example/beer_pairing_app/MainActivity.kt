4afbad82f79fdf9647d375999ed73d1c135c44ba
mi_chol <mikescottvaldez@gmail.com>
2019-12-11 02:51:35 -0800

    updating/adding new files taken from andysbranch

--- app/src/main/java/com/example/beer_pairing_app/MainActivity.kt
+++ app/src/main/java/com/example/beer_pairing_app/MainActivity.kt
@@ -2,11 +2,88 @@ package com.example.beer_pairing_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.beer_pairing_app.databinding.ActivityMainBinding
+import com.google.gson.GsonBuilder
+import kotlinx.android.synthetic.main.activity_main.*
+import okhttp3.*
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+//        // The following lines have been commented out due to recycler view testing
+//        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+//        val navController = this.findNavController(R.id.myNavHostFragment)
+//        NavigationUI.setupActionBarWithNavController(this, navController)
+
         setContentView(R.layout.activity_main)
+        recyclerView_main.layoutManager = LinearLayoutManager(this)
+        fetchJson()
     }
+
+//    // The following function was commented out due to recycler view testing
+//    override fun onSupportNavigateUp(): Boolean {
+//        val navController = this.findNavController(R.id.myNavHostFragment)
+//        return navController.navigateUp()
+//    }
+
+    private fun fetchJson() {
+
+        println("Attempting to fetch JSON")
+
+        // URL for beers api (Returns 25 beers)
+        //val url = "https://api.punkapi.com/v2/beers"
+        val url = "https://api.punkapi.com/v2/beers?food=spicy"
+
+        // Creating okHttpClient for fetching from API
+        val client = OkHttpClient()
+
+        // Creating a request
+        val request = Request.Builder().url(url).build()
+
+        // Can't run execute on main thread so enqueue is used instead
+        client.newCall(request).enqueue(object: Callback {
+            override fun onResponse(call: Call, response: Response) {
+                val body = response.body?.string()
+
+                println("successful fetch")
+
+                // printing body object for testing purposes
+                //println(body)
+
+                val gson = GsonBuilder().create()
+
+                // Link demonstrates how to create list from JSON object
+                // https://stackoverflow.com/questions/53685627/expected-begin-object-but-was-begin-array-kotlin
+                val beers: List<Beer> = gson.fromJson(body, Array<Beer>::class.java).toList()
+                runOnUiThread {
+                    recyclerView_main.adapter = MainAdapter(beers)
+                }
+
+//                // Iterating through gson created object to check results
+//                beers.forEach{
+//                    println(it.id)
+//                    println("Beer name: " + it.name)
+//                    println("Tagline: " + it.tagline)
+//                    println("description: " + it.description)
+//                }
+
+            }
+            override fun onFailure(call: Call, e: IOException) {
+                println("failed to execute")
+            }
+        })
+
+    }
+
 }
+
+class Beer(val id: Int, val name: String, val tagline: String, val description: String, val abv: String, val food_pairing: Array<String>)
---
+++ app/src/main/java/com/example/beer_pairing_app/MainAdapter.kt
@@ -0,0 +1,62 @@
+package com.example.beer_pairing_app
+
+import android.graphics.Color
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.beer_row.view.*
+
+class MainAdapter(val beers: List<Beer>): RecyclerView.Adapter<CustomViewHolder>() {
+
+    // Getting the number of beers per request
+    override fun getItemCount(): Int {
+        return beers.size
+    }
+
+    // Inflating layout to include a beer picture and description
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CustomViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val cellForRow = layoutInflater.inflate(R.layout.beer_row, parent, false)
+        return CustomViewHolder(cellForRow)
+    }
+
+    // Changing contents of xml file based on request results
+    override fun onBindViewHolder(holder: CustomViewHolder, position: Int) {
+
+        val beer = beers[position]
+
+
+        if(position % 2 == 0){
+            holder.view.beer_card.setBackgroundColor(Color.rgb(250,217,237))
+        }
+
+        val name = beer.name
+        holder.view.textView_beer_title.text = name
+
+        val tagline = beer.tagline
+        holder.view.textView_beer_tagline.text = tagline
+
+        val description = beer.description
+        holder.view.textView_beer_description.text = "Taste: $description"
+
+        val abv = beer.abv
+        holder.view.textView_beer_abv.text = "Alcohol by volume: $abv"
+
+        // Suggested foods are stored in an array, so we have to parse array to a single string
+        val foods = beer.food_pairing
+
+        // joinToString parses array into a single string using a custom separator
+        val allFoods = foods.joinToString(", ")
+
+        // updating text for food pairing text view
+        val suggestedFoods = "Eat with:  $allFoods"
+        holder.view.textView_beer_food_pairing.text = suggestedFoods
+    }
+
+}
+
+// CustomViewHolder class on the fly
+class CustomViewHolder(val view: View): RecyclerView.ViewHolder(view) {
+
+}
