009279fb268021c9eb0e894d412a5d02a131f3a2
andeechu <andychuabsurd@gmail.com>
2019-12-12 01:40:45 -0800

    added activity switching for buttons

---
+++ app/src/main/java/com/example/beer_pairing_app/BeersList.kt
@@ -0,0 +1,32 @@
+package com.example.beer_pairing_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.beer_pairing_app.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_beerlist.*
+
+class BeersList : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    lateinit var viewModel: MainViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_beerlist)
+        val passedCategory: String = intent.getStringExtra("category")
+        recyclerView_main.layoutManager = LinearLayoutManager(this)
+        val topSpacingDecoration = TopSpacingItemDecoration(30)
+        recyclerView_main.addItemDecoration(topSpacingDecoration)
+
+        viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
+        viewModel.beers.observe(this, Observer{
+            println(it.toList())
+            recyclerView_main.adapter = MainAdapter(it.toList())
+        })
+
+        viewModel.setCategory(passedCategory)
+    }
+}
--- app/src/main/java/com/example/beer_pairing_app/MainActivity.kt
+++ app/src/main/java/com/example/beer_pairing_app/MainActivity.kt
@@ -1,52 +1,49 @@
 package com.example.beer_pairing_app
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.ViewModelProviders
-import androidx.navigation.findNavController
-import androidx.navigation.ui.NavigationUI
-import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.beer_pairing_app.databinding.ActivityMainBinding
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    lateinit var viewModel: MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
+        val spicyButton = findViewById<Button>(R.id.spicy)
+        spicyButton.setOnClickListener{
+            val intent: Intent = Intent(applicationContext, BeersList::class.java)
+            intent.putExtra("category", "spicy")
+            startActivity(intent)
+        }
 
-        // The following lines have been commented out due to recycler view testing
-//        val navController = this.findNavController(R.id.myNavHostFragment)
-//        NavigationUI.setupActionBarWithNavController(this, navController)
+        val sweetButton = findViewById<Button>(R.id.sweet)
+        sweetButton.setOnClickListener{
+            val intent: Intent = Intent(applicationContext, BeersList::class.java)
+            intent.putExtra("category", "sweet")
+            startActivity(intent)
+        }
 
+        val sourButton = findViewById<Button>(R.id.sour)
+        sourButton.setOnClickListener{
+            val intent: Intent = Intent(applicationContext, BeersList::class.java)
+            intent.putExtra("category", "sour")
+            startActivity(intent)
+        }
 
-        recyclerView_main.layoutManager = LinearLayoutManager(this)
-        val topSpacingDecoration = TopSpacingItemDecoration(30)
-        recyclerView_main.addItemDecoration(topSpacingDecoration)
-
-        viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
-        viewModel.beers.observe(this, Observer{
-            println(it.toList())
-            recyclerView_main.adapter = MainAdapter(it.toList())
-        })
-
-        viewModel.setCategory("sweet")
+        val bitterButton = findViewById<Button>(R.id.bitter)
+        bitterButton.setOnClickListener{
+            val intent: Intent = Intent(applicationContext, BeersList::class.java)
+            intent.putExtra("category", "bitter")
+            startActivity(intent)
+        }
 
     }
-
-
-//    // The following function was commented out due to recycler view testing
-//    override fun onSupportNavigateUp(): Boolean {
-//        val navController = this.findNavController(R.id.myNavHostFragment)
-//        return navController.navigateUp()
-//    }
 
 }
